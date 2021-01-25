2e1c8bc290d19315b55504274629434997f29deb
João Moraes <joaovitordmoraes@gmail.com>
2021-01-25 19:49:37 -0300

    :sparkles: Update metatags

--- pages/_app.js
+++ pages/_app.js
@@ -43,13 +43,13 @@ export default function App({ Component, pageProps }) {
           <meta name="description" content={db.description} />
 
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://theoffice-aluraquiz-git-main.joaovitordmoraes.vercel.app/" />
+          <meta property="og:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
           <meta property="og:title" content={db.title} />
           <meta property="og:description" content={db.description} />
           <meta property="og:image" content={db.bg} />
 
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://theoffice-aluraquiz-git-main.joaovitordmoraes.vercel.app/" />
+          <meta property="twitter:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
           <meta property="twitter:title" content={db.title} />
           <meta property="twitter:description" content={db.description} />
           <meta property="twitter:image" content={db.bg}></meta>
