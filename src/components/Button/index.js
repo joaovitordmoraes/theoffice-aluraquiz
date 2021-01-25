2e9ac3f62b6eb5a7f9cd7ca048c986b7cc442eb9
João Moraes <joaovitordmoraes@gmail.com>
2021-01-25 19:24:15 -0300

    :sparkles: Create Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background-color: ${({ theme }) => theme.colors.button};
+    padding: 10px 16px;
+    border: none;
+    border-radius: ${({ theme }) => theme.borderRadius};
+    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.contrastText};
+    text-transform: uppercase;
+    width: 100%;
+    outline: none;
+    cursor: pointer;
+    transition: all 250ms linear;
+
+    &:hover,
+    &:focus {
+        background-color: ${({ backgroundColor }) => backgroundColor};
+        transition: all 250ms linear;
+    }
+`
+
+export default Button
