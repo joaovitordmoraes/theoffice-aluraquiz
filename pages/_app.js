46ed48ce1bfa8fe9bfd51b2291e838eac31d24df
João Moraes <joaovitordmoraes@gmail.com>
2021-01-25 19:22:23 -0300

    :sparkles: Add metatags, GlobalStyles and theme config

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,59 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Head from 'next/head'
+import db from '../db.json'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+      box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+
+  html, body {
+    min-height: 100vh;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Head>
+          <title>{db.title}</title>
+
+          <meta name="title" content={db.title} />
+          <meta name="description" content={db.description} />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://metatags.io/" />
+          <meta property="og:title" content={db.title} />
+          <meta property="og:description" content={db.description} />
+          <meta property="og:image" content={db.bg} />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://metatags.io/" />
+          <meta property="twitter:title" content={db.title} />
+          <meta property="twitter:description" content={db.description} />
+          <meta property="twitter:image" content={db.bg}></meta>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
