8a0681eadf3c1b9f42c8009d4aa63451e8620365
João Moraes <joaovitordmoraes@gmail.com>
2021-01-26 23:17:00 -0300

    :art: Format the code with ESLint

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import Head from 'next/head'
-import db from '../db.json'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -27,33 +28,36 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>{db.title}</title>
+
+        <meta name="title" content={db.title} />
+        <meta name="description" content={db.description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
+        <meta property="og:title" content={db.title} />
+        <meta property="og:description" content={db.description} />
+        <meta property="og:image" content={db.bg} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
+        <meta property="twitter:title" content={db.title} />
+        <meta property="twitter:description" content={db.description} />
+        <meta property="twitter:image" content={db.bg}></meta>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Head>
-          <title>{db.title}</title>
-
-          <meta name="title" content={db.title} />
-          <meta name="description" content={db.description} />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
-          <meta property="og:title" content={db.title} />
-          <meta property="og:description" content={db.description} />
-          <meta property="og:image" content={db.bg} />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://theoffice-aluraquiz.joaovitordmoraes.vercel.app/" />
-          <meta property="twitter:title" content={db.title} />
-          <meta property="twitter:description" content={db.description} />
-          <meta property="twitter:image" content={db.bg}></meta>
-        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
