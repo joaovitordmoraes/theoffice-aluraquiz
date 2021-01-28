228abfca38f54317be7e41bdfce51d7b1db56b13
João Moraes <joaovitordmoraes@gmail.com>
2021-01-27 23:17:00 -0300

    :sparkles: Create Topic on Widget component

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+/* eslint-disable linebreak-style */
+import styled from 'styled-components';
 
 const Widget = styled.div`
   margin-top: 24px;
@@ -20,7 +21,7 @@ const Widget = styled.div`
     font-weight: 400;
     line-height: 1;
   }
-`
+`;
 
 Widget.Header = styled.header`
   display: flex;
@@ -32,7 +33,7 @@ Widget.Header = styled.header`
   * {
     margin: 0;
   }
-`
+`;
 
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
@@ -49,6 +50,24 @@ Widget.Content = styled.div`
     list-style: none;
     padding: 0;
   }
-`
+`;
 
-export default Widget
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: 300ms;
+  display: block;
+
+  &:hover,
+  &:focus {
+    opacity: .5; 
+  }
+`;
+
+export default Widget;
