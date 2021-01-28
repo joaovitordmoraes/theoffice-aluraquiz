06bdc95c9d0f6ce6f44405d2a415b1cf6991f408
João Moraes <joaovitordmoraes@gmail.com>
2021-01-27 23:17:42 -0300

    :art: Update Quiz page structure

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,34 +1,145 @@
-import React from 'react';
+/* eslint-disable linebreak-style */
+/* eslint-disable react/require-default-props */
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import db from '../db.json';
 
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
+import Button from '../src/components/Button';
 import Footer from '../src/components/Footer';
 import GithubCorner from '../src/components/GithubCorner';
 
-export default function QuizPage() {
+function LoadingScreen() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
 
-    return(
-        <QuizBackground backgroundImage={db.bg}>
-            <QuizContainer>
-                <QuizLogo />
-
-                <Widget>
-                    <Widget.Header>
-                        <h1>{db.title}</h1>
-                    </Widget.Header>
-
-                    <Widget.Content>
-                        <p>Conteúdo em desenvolvimento</p>
-                    </Widget.Content>
-                </Widget>
-
-                <Footer />
-            </QuizContainer>
-
-            <GithubCorner projectUrl="https://github.com/joaovitordmoraes/theoffice-aluraquiz" />
-        </QuizBackground>
-    );
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
 }
+
+function QuestionWidget({ question, totalQuestions, questionIndex, onSubmit }) {
+  const questionId = `question__${questionIndex}`;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        src="https://placehold.it/400x400"
+        alt="Descrição da imagem aqui"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+      />
+
+      <Widget.Content>
+        <h2>{question.title}</h2>
+
+        <p>{question.description}</p>
+
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          onSubmit();
+        }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+
+            return (
+              <Widget.Topic as="label" htmlFor={alternativeId} key={alternativeId}>
+                <input
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
+export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setcurrentQuestion] = useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmit() {
+    const nextQuestion = questionIndex + 1;
+
+    if (nextQuestion < totalQuestions) {
+      setcurrentQuestion(questionIndex + 1);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmit}
+          />
+        )}
+
+        {screenState === screenStates.LOADING && <LoadingScreen />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+
+        <Footer />
+      </QuizContainer>
+
+      <GithubCorner projectUrl="https://github.com/joaovitordmoraes/theoffice-aluraquiz" />
+    </QuizBackground>
+  );
+}
+
+QuestionWidget.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  question: PropTypes.object,
+  totalQuestions: PropTypes.number,
+  questionIndex: PropTypes.number,
+  onSubmit: PropTypes.func,
+};
